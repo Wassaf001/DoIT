38f4f03cf55418914f7ff03a912208d221ad3cee
wassaf <wassafali213@gmail.com>
2024-03-28 22:33:33 +0530

    RESET

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PagesComponent } from './CustomComponents/pages/pages.component';
+import { TodosComponent } from './CustomComponents/todos/todos.component';
+import { AddTodoComponent } from './CustomComponents/add-todo/add-todo.component';
+import { TodoItemComponent } from './CustomComponents/todo-item/todo-item.component';
+import { OnepageComponent } from './CustomComponents/onepage/onepage.component';
+import { LoginComponent } from './CustomComponents/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PagesComponent,
+    TodosComponent,
+    AddTodoComponent,
+    TodoItemComponent,
+    OnepageComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
